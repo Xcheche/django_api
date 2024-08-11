b6805e32155a6fff601051bc4c490129407f5186
xcheche <chekwubechukwu23@gmail.com>
2024-08-11 20:14:24 +0100

    created models for api

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from courses.models import Course
 # Register your models here.
+
+
+
+class CourseAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Names of Courses", {"fields": ["name"]}),
+        ("Programming Languages", {"fields": ["language"]}),
+        ("Prices", {"fields": ["price"]}),
+    ]
+
+
+
+
+admin.site.register(Course, CourseAdmin)
