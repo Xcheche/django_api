d32a2e834be11850a2eb27ea2a71871960757a15
xcheche <chekwubechukwu23@gmail.com>
2024-08-15 08:05:51 +0100

    added serializers

--- courses/models.py
+++ courses/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Course(models.Model):
     name = models.CharField(max_length=200)
     language = models.CharField(max_length=100)
-    price = models.CharField(max_length=10)
+    price = models.CharField(max_length=10,default=0)
     
     def __str__(self):
         return self.name
